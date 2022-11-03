83b3b8c452a12cf82ba1d26d01bd21485b9e8edc
tobySolutions <adedejitobiloba7@gmail.com>
2022-11-03 19:13:23 +0100

    first commit

---
+++ file1.js
@@ -0,0 +1,8 @@
+// Write a JavaScript program to compute the sum of the two given integers. If the two values are same, then returns triple their sum.
+
+// Write a JavaScript program to create a new string adding "Py" in front of a given string. If the given string begins with "Py" then return the original string.
+let stringa = "password"
+console.log(stringa[4])
+// for (let i = 0; i < stringa.length; i++){
+//     console.log(stringa[i])
+// }
